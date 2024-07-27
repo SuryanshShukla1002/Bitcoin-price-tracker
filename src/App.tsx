15f4fc6b27bc9c407ebdfda56c726e911e2d0d23
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-07-27 23:17:12 +0530

    input method created

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AmountInput from "./AmountInput";
 
 function App() {
   const [amount, setAmount] = useState("");
@@ -8,7 +9,12 @@ function App() {
         <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30% ">
           Find cheapest BTC
         </h1>
-        
+        <div className="flex justify-center mt-8">
+          <AmountInput
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
       </main>
     </>
   );
