615fa0738d2d2ebea5e1f02088999b1232ed4301
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-07-27 23:17:51 +0530

    input structure type defining

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,9 +1,20 @@
+import { ChangeEventHandler } from "react";
 
+export type InputProps = {
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  className?: string;
+  placeholder?: string;
+};
 
-export default function Input(props) {
+export default function Input(props: InputProps) {
+  return (
     <input
-          type="text"
-          value={props.value}
-          onChange={props.onChange}
-        />
+      type="text"
+      placeholder={props.placeholder || ""}
+      className={"border  border-white/10 bg-blue-950 p-2" + props.className}
+      value={props.value}
+      onChange={props.onChange}
+    />
+  );
 }
