3d6f6d7c3015bdb390349dbb417eb0a69ae72f6c
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-01 12:05:24 +0530

    Result rows created more

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import AmountInput from "./AmountInput";
+import ResultRow from "./ResultRow";
 
 function App() {
-  const [amount, setAmount] = useState("");
+  const [amount, setAmount] = useState("100");
   return (
     <>
       <main className="max-w-2xl px-4 py-8 mx-auto">
         <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30% ">
           Find cheapest BTC
         </h1>
-        <div className="flex justify-center mt-8">
+        <div className="flex justify-center mt-6">
           <AmountInput
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
           />
         </div>
+        <div>
+          <div className="mt-6">
+            <ResultRow/>
+            <ResultRow/>
+            <ResultRow/>
+            <ResultRow/>
+          </div>
+        </div>
       </main>
     </>
-  )
+  );
 }
 
 export default App;
