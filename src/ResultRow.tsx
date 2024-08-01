a39a35fdf7a9492fe18cac9a099af1850cb848f5
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-01 12:06:08 +0530

    provider name and default BTC price added

--- src/ResultRow.tsx
+++ src/ResultRow.tsx
@@ -1,7 +1,14 @@
 export default function ResultRow() {
-    return (
-        <div className="border min-h-12">
-            test row
+  return (
+    <div className="border min-h-12 border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 ">
+      <div className="flex gap-4">
+        <div>logi</div>
+        <div className="grow">Provider name</div>
+        <div className="flex gap-2">
+          <span className="text-xl text-purple-200/80"> 0.000 </span>
+          <span className="text-xl text-purple-300/50"> BTC</span>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
